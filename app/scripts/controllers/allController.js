b0b49f7b98ff0a405714eb936cd5392337358b5f
aco <wwsxuan@163.com>
2016-09-18 12:48:24 +0800

    小调整

--- app/scripts/controllers/allController.js
+++ app/scripts/controllers/allController.js
@@ -3,12 +3,13 @@
 define(function () {
 
     /* 整个页面最大的controller */
-    var controller = function ($scope) {
+    var controller = function ($scope, $location) {
         /* controller code here */
+        $location.path('/home');
     };
 
 
-    controller.$inject = ['$scope'];
+    controller.$inject = ['$scope', '$location'];
 
     return controller;
 });
--- app/scripts/require-config.js
+++ app/scripts/require-config.js
@@ -4,8 +4,8 @@ require.config({
     baseUrl: 'scripts',
 
     paths: {
-        'angular': '../lib/bower/angular/angular',
-        'angular.ui.router': '../lib/bower/angular-ui-router/release/angular-ui-router',
+        'angular': '../lib/bower/angular/angular.min',
+        'angular.ui.router': '../lib/bower/angular-ui-router/release/angular-ui-router.min',
         'text': '../lib/bower/requirejs-text/text',
         'template': '../template'
     },
